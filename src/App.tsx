6d667a36e44deae3a5ae5d09c0fe92788f4094c0
zhekaasd <zhekaasd13@gmail.com>
2024-03-10 16:56:55 +0400

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="header">logotype</header>
+      <nav className="sidebar">
+        <ul>
+          <li>Profile</li>
+          <li>Messages</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+      <main className="main"></main>
     </div>
   );
 }
